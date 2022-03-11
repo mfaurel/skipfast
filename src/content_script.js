6a5ec7612024d4cef9f23e03be68e479b836e648
Your Name <you@example.com>
2022-03-11 12:08:06 +0100

    New logos and new algo

--- src/content_script.js
+++ src/content_script.js
@@ -1,23 +1,49 @@
-const INTRO_UIA = "player-skip-intro";
-const RECAP_UIA = "player-skip-recap";
-const NEXT_UIA = "next-episode-seamless-button";
-const NEXT_DRAIN_UIA = "next-episode-seamless-button-draining";
-const NETFLIX = 'Netflix';
-const PRIME = 'Prime Video';
-const DISNEY = 'Disney+';
+const NETFLIX_INTRO_UIA = "player-skip-intro";
+const NETFLIX_RECAP_UIA = "player-skip-recap";
+const NETFLIX_NEXT_UIA = "next-episode-seamless-button";
+const NETFLIX_NEXT_DRAIN_UIA = "next-episode-seamless-button-draining";
 
-const BUTTONS = [INTRO_UIA, RECAP_UIA, NEXT_UIA, NEXT_DRAIN_UIA];
+const DISNEY_NEXT_EPISODE_TEST_ID = "up-next-play-button";
+const DISNEY_INTRO_CLASS = "skip__button";
+
+const PRIME_RECAP = "skipElement";
+const PRIME_RECAP_2 = "atvwebplayersdk-skipelement-button";
+const PRIME_RECAP_3 = "fqye4e3";
+const PRIME_ADS = "adSkipButton";
+const PRIME_ADS_2 = "afu4rd6c";
+const PRIME_ADS_3 = "f1cw2swo";
+const PRIME_CREDITS = "f1oordr3";
+const PRIME_NEXT = "fd8k8m6";
+
+const BUTTONS = [NETFLIX_INTRO_UIA, NETFLIX_RECAP_UIA, NETFLIX_NEXT_UIA, NETFLIX_NEXT_DRAIN_UIA];
+const BUTTONS_ID = [DISNEY_NEXT_EPISODE_TEST_ID];
+const BUTTONS_CLASS = [DISNEY_INTRO_CLASS, PRIME_RECAP, PRIME_RECAP_2, PRIME_RECAP_3, PRIME_ADS, PRIME_ADS_2, PRIME_ADS_3, PRIME_CREDITS, PRIME_NEXT];
+const LINK_CLASS = [PRIME_RECAP, PRIME_RECAP_2, PRIME_RECAP_3, PRIME_ADS, PRIME_ADS_2, PRIME_ADS_3, PRIME_CREDITS, PRIME_NEXT];
+
 
 async function skipper() {
   try {
     chrome.storage.local.get(
-      ["skipIntro", "skipRecap", "skipNext"],
-      ({ skipIntro, skipRecap, skipNext }) => {
+      ["skipIntro", "skipRecap", "skipNext", "skipAds"],
+      ({ skipIntro, skipRecap, skipNext, skipAds }) => {
         const mapper = {
-          [INTRO_UIA]: skipIntro,
-          [RECAP_UIA]: skipRecap,
-          [NEXT_UIA]: skipNext,
-          [NEXT_DRAIN_UIA]: skipNext,
+          // Netflix
+          [NETFLIX_INTRO_UIA]: skipIntro,
+          [NETFLIX_RECAP_UIA]: skipRecap,
+          [NETFLIX_NEXT_UIA]: skipNext,
+          [NETFLIX_NEXT_DRAIN_UIA]: skipNext,
+          // Prime Video
+          [PRIME_RECAP]: skipRecap,
+          [PRIME_RECAP_2]: skipRecap,
+          [PRIME_RECAP_3]: skipRecap,
+          [PRIME_ADS]: skipAds,
+          [PRIME_ADS_2]: skipAds,
+          [PRIME_ADS_3]: skipAds,
+          [PRIME_CREDITS]: skipNext,
+          [PRIME_NEXT]: skipNext,
+          // DisneyPlus
+          [DISNEY_INTRO_CLASS]: skipIntro,
+          [DISNEY_NEXT_EPISODE_TEST_ID]: skipNext
         };
         BUTTONS.forEach((uia) => {
           const button = Object.values(
@@ -25,6 +51,34 @@ async function skipper() {
           ).find((elem) => elem.getAttribute("data-uia") === uia);
           if (button && mapper[uia]) {
             button.click();
+            // console.log("Skipped " + uia);
+          }
+        });
+        BUTTONS_ID.forEach((id) => {
+          const button = Object.values(
+            document.getElementsByTagName("button")
+          ).find((elem) => elem.getAttribute("data-testid") === id);
+          if (button && mapper[id]) {
+            button.click();
+            // console.log("Skipped " + id);
+          }
+        });
+        BUTTONS_CLASS.forEach((id) => {
+          const button = Object.values(
+            document.getElementsByTagName("button")
+          ).find((elem) => elem.classList.contains(id));
+          if (button && mapper[id]) {
+            button.click();
+            // console.log("Skipped " + id);
+          }
+        });
+        BUTTONS_CLASS.forEach((clazz) => {
+          const link = Object.values(
+            document.getElementsByTagName("div")
+          ).find((elem) => elem.classList.contains(clazz));
+          if (link && mapper[clazz]) {
+            link.click();
+            // console.log("Skipped " + clazz);
           }
         });
       }
@@ -34,100 +88,6 @@ async function skipper() {
   }
 }
 
-if (document.location.host.includes(".netflix.")) {
+if (document.location.host.includes(".netflix.") || document.location.host.includes(".primevideo.") || document.location.host.includes(".disneyplus.")) {
   setInterval(() => skipper(), 500);
-}
-
-async function skippy() {
-  try {
-    const skipSelectors = [
-      // Netflix
-      {
-        platform: NETFLIX,
-        info: 'Skipping credits.',
-        selector: '.skip-credits > a'
-      },
-      {
-        platform: NETFLIX,
-        info: 'Playing next episode.',
-        selector: '[data-uia="next-episode-seamless-button"]'
-      },
-      {
-        platform: NETFLIX,
-        info: 'Continue playing.',
-        selector: `[aria-label='Continue Playing']`
-      },
-      {
-        platform: NETFLIX,
-        info: 'Skipping recap.',
-        selector: `[aria-label='Skip Recap']`
-      },
-      {
-        platform: NETFLIX,
-        info: 'Skipping intro.',
-        selector: `[aria-label='Skip Intro']`
-      },
-      // Prime Video
-      {
-        platform: PRIME,
-        info: 'Skipping credits.',
-        selector: '.f1oordr3'
-      },
-      {
-        platform: PRIME,
-        info: 'Playing next episode.',
-        selector: '.fd8k8m6'
-      },
-      {
-        platform: PRIME,
-        info: 'Skipping ad.',
-        selector: '.adSkipButton'
-      },
-      {
-        platform: PRIME,
-        info: 'Skipping recap.',
-        selector: '.skipElement'
-      },
-      {
-        platform: PRIME,
-        info: 'Skipping recap.',
-        selector: '.atvwebplayersdk-skipelement-button'
-      },
-      {
-        platform: PRIME,
-        info: 'Skipping recap.',
-        selector: '.fqye4e3'
-      },
-//  fqye4e3 fovm8oe fez7z67 fektfsf fif0hcs f177tia9 fww9brl f1nxf0rp f1ylp05h atvwebplayersdk-skipelement-button f1cg7427 f989gul f1rjin6j f19qnh9o
-      // Disney Plus
-      {
-        platform: DISNEY,
-        info: 'Skipping credits',
-        selector: '.skip__button'
-      },
-      {
-        platform: DISNEY,
-        info: 'Playing next episode',
-        selector: 'button.sc-gipzik.play'
-      },
-    ];
-    for (skipSelector of skipSelectors) {
-      const skipButton = document.querySelector(skipSelector.selector);
-      if (skipButton) {
-        skipButton.click();
-        console.log(`${skipSelector.platform}: ${skipSelector.info}`);
-        break;
-      }
-    }
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-if (document.location.host.includes(".primevideo.")) {
-  setInterval(() => skippy(), 500);
-}
-
-if (document.location.host.includes(".disneyplus.")) {
-  setInterval(() => skippy(), 500);
 }
--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,7 @@
 let skipIntroCheckbox = document.getElementById("skip-intro");
 let skipRecapCheckbox = document.getElementById("skip-recap");
 let skipNextCheckbox = document.getElementById("skip-next");
+let skipAdsCheckbox = document.getElementById("skip-ads");
 
 skipIntroCheckbox.addEventListener("click", async () => {
   chrome.storage.local.set({ skipIntro: skipIntroCheckbox.checked });
@@ -14,9 +15,14 @@ skipNextCheckbox.addEventListener("click", async () => {
   chrome.storage.local.set({ skipNext: skipNextCheckbox.checked });
 });
 
+skipAdsCheckbox.addEventListener("click", async () => {
+  chrome.storage.local.set({ skipAds: skipAdsCheckbox.checked });
+});
+
+
 chrome.storage.local.get(
-  ["skipIntro", "skipRecap", "skipNext"],
-  ({ skipIntro, skipRecap, skipNext }) => {
+  ["skipIntro", "skipRecap", "skipNext", "skipAds"],
+  ({ skipIntro, skipRecap, skipNext, skipAds }) => {
     if (skipIntro) {
       skipIntroCheckbox.checked = true;
     }
@@ -25,6 +31,9 @@ chrome.storage.local.get(
     }
     if (skipNext) {
       skipNextCheckbox.checked = true;
+    }
+    if (skipAds) {
+      skipAdsCheckbox.checked = true;
     }
   }
 );
